50d837e985919c2ad0918e3b03dfe89dd3cbeeac
evan <thetyphothanian@gmail.com>
2025-06-13 17:35:34 -0600

    switched to fabric to be able to use doctor4t's mods

---
+++ src/client/java/net/typho/tungsten/TungstenClient.java
@@ -0,0 +1,10 @@
+package net.typho.tungsten;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TungstenClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
